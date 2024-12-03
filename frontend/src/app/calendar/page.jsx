2caa00d7cac0b9117bdbb9cfa2882cf21c558763
Benjamin Karlsson <smileben2012@gmail.com>
2024-12-04 00:39:31 +0100

    fixed dates

--- frontend/src/app/calendar/page.jsx
+++ frontend/src/app/calendar/page.jsx
@@ -2,25 +2,22 @@ import React from "react";
 import Day from "./components/day";
 
 export default function Home() {
-  var date = new Date();
+  let date = new Date();
 
-  // add a day
- 
+  let dates = [date];
 
-  
+  // Add the next 6 days
+  for (let i = 1; i <= 6; i++) {
+    let nextDate = new Date(date);
+    nextDate.setDate(date.getDate() + i);
+    dates.push(nextDate);
+  }
 
-
-  
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Day date={date}/>
-      <Day date={date} />
-      <Day date={date} />
-      <Day date={date} />
-      <Day date={date} />
-      <Day date={date} />
-      <Day date={date} />
-      
+      {dates.map((date, index) => (
+        <Day key={index} date={date} />
+      ))}
     </div>
   );
 }
